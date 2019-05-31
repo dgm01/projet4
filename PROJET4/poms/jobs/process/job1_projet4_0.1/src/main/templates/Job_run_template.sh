60ce623a2734c92abc6a48fcfacde6a9ede8c387
Jean-François Lamballe <jflamballe@talentsoft.com>
2019-05-31 12:16:23 +0200

    remove ignored files

--- PROJET4/poms/jobs/process/job1_projet4_0.1/src/main/templates/Job_run_template.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd `dirname $0`
-ROOT_PATH=`pwd`
-java -Dtalend.component.manager.m2.repository=$ROOT_PATH/../lib -Xms256M -Xmx1024M -Dfile.encoding=UTF-8 -cp .:$ROOT_PATH:$ROOT_PATH/../lib/routines.jar:$ROOT_PATH/../lib/dom4j-1.6.1.jar:$ROOT_PATH/../lib/log4j-1.2.17.jar:$ROOT_PATH/job1_projet4_0_1.jar: projet4.job1_projet4_0_1.job1_projet4  "$@"
